74c1540700ea82056d196b63f89b8efba7a53333
Glori <128950766+GlorianaZelaya@users.noreply.github.com>
2023-07-11 23:08:01 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [todo, setTodo] = useState("")
+	const [todolist, setTodolist] = useState([])
+	const [removeshow, setremoveshow] = useState(false);
+	/* const remove = todolist.filter((item, id) => index != id)
+	setTodolist(remove) */
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center mt-4">♥ To Do ♥</h1>
+			<input type="text" placeholder=" What needs to be done?" value={todo}
+				onChange={(e) => {
+					setTodo(e.target.value)
+				}
+				}
+				onKeyDown={(e) => {
+					e.key === "Enter" ? setTodolist(todolist.concat(todo))
+						: null;
+					e.key === "Enter" ? setTodo("")
+						: null;
+				}
+				}></input>
+			{todolist.map((task, index) => (
+				<li
+					key={index}
+					onMouseEnter={() => setremoveshow(index)}
+					onMouseLeave={() => setremoveshow(null)}>
+					{task}{""}
+					{removeshow == index && (
+						<img
+							src="https://play-lh.googleusercontent.com/clvbyNWTT8fYSfdTvrZM4M68R-9JRtFfIQmt3YZr90pkph4WJ6C2JUfjDL1CnQVRHkQZ"
+							onClick={() => {
+								setTodolist(
+									todolist.filter(
+										(a, id) => id !== index
+									)
+								);
+							}}></img>
+					)}</li>
+			))}
+			<li className="TaskLeft">
+				{todolist.length} items left
+			</li>
 		</div>
 	);
 };
